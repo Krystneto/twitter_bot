35c9784aa8c99ff748a9d1e6538e549ea79e9985
Kristopher Parrillo <kkparrillo@gmail.com>
2017-07-21 10:25:35 -0700

    adds keys for twitter access

---
+++ app.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+var TwitterPackage = require('twitter');
+
+// replace the words in caps with the keys that
+// we saw before on apps.twitter.com
+var secret = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_TOKEN_SECRET
+}
+
+var Twitter = new TwitterPackage(secret);
+
+var query = "alot";
+Twitter.get('search/tweets', {q: query, count: 1, lang:"en"}, function(error, tweets, response) {
+   
+   var tweet_list = tweets['statuses'];
+   
+   for (var i = 0; i < tweet_list.length; i++) {
+   		if ('retweeted_status' in tweet_list[i]) {
+   			continue;
+   		} 
+   		var screen_name = tweet_list[i].user.screen_name;
+   		var message = "@" + screen_name + " Alot confused, a lot not understand feelings";
+   		var tweet_id = tweet_list[i].id_str;
+
+   		try {
+		    Twitter.post('statuses/update', {"status": message, "in_reply_to_status_id":tweet_id}, function(error, tweet, response){
+			     console.log("Tweet posted successfully!")
+  			});
+  		}
+
+  		catch(err) {
+  		    console.log(err);
+  		}
+   }
+});
